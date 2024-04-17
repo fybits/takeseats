e8a29cb3f7f82eb43cf389583e261891ada075d5
fybits <sergeig1313@gmail.com>
2024-04-17 16:05:56 +0300

    Add new card creation menu

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 
-import { Application, autoDetectRenderer } from 'pixi.js';
+import { Application, Assets, Texture, autoDetectRenderer } from 'pixi.js';
 import { PeerRoom } from './PeerRoom';
 import Controls from './Controls';
 import Camera from './game-objects/Camera';
 import GameManager from './GameManager';
+import Card from './game-objects/Card';
 
 declare global {
     var gm: GameManager;
@@ -20,6 +21,12 @@ const hostBtn = document.querySelector<HTMLButtonElement>('button#host')!;
 const lobbyInput = document.querySelector<HTMLInputElement>('input#lobby')!;
 const joinBtn = document.querySelector<HTMLButtonElement>('button#join')!;
 
+const createCardForm = document.querySelector<HTMLFormElement>('#card-dialog')!;
+
+const closeBtns = document.querySelectorAll<HTMLInputElement>('.close');
+closeBtns.forEach((btn) => {
+    btn.onclick = (e) => (e.target as HTMLElement).parentElement!.hidden = true;
+})
 
 const storedNickname = localStorage.getItem('nickname');
 if (storedNickname) {
@@ -28,6 +35,15 @@ if (storedNickname) {
 
 let room: PeerRoom | null = null;
 
+function getBase64(file: File): Promise<string> {
+    const reader = new FileReader()
+    return new Promise(resolve => {
+        reader.onload = ev => {
+            resolve(reader.result as string)
+        }
+        reader.readAsDataURL(file)
+    })
+}
 
 (async () => {
 
@@ -57,6 +73,24 @@ let room: PeerRoom | null = null;
             const syncBtn = document.querySelector<HTMLButtonElement>("#sync-btn")!;
             syncBtn.hidden = false;
             syncBtn.addEventListener('click', () => gameManager.sync())
+            const cardBtn = document.querySelector<HTMLButtonElement>("#create-card-btn")!;
+            cardBtn.hidden = false;
+            cardBtn.addEventListener('click', () => {
+                createCardForm.hidden = false;
+            });
+            createCardForm.onsubmit = async (e) => {
+                e.preventDefault();
+                const formData = new FormData(createCardForm);
+                const faceFile = formData.get('face') as File;
+                const backFile: File = formData.get('back') as File;
+                const [faceUrl, backUrl] = await Promise.all([getBase64(faceFile), getBase64(backFile)]);
+                console.log(faceFile, backFile)
+                Assets.add({ alias: faceFile.name, src: faceUrl })
+                Assets.add({ alias: backFile.name, src: backUrl })
+                await Assets.load<Texture>([faceFile.name, backFile.name]);
+                const card = new Card(Assets.get<Texture>(faceFile.name), Assets.get<Texture>(backFile.name));
+                gameManager.camera.addChild(card);
+            }
         }
         globalThis.gm = gameManager;
 
