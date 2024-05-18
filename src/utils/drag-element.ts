95a5f4391a983c3b51a7bcfa1b8419d005262750
fybits <sergeig1313@gmail.com>
2024-05-18 06:52:11 +0300

    Refactor drag elements util

--- src/utils/drag-element.ts
+++ src/utils/drag-element.ts
@@ -1,44 +1,40 @@
-export default function dragElement<T extends HTMLElement>(dialog: T): T {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    const header = dialog.querySelector<HTMLDivElement>(".dialog-header")!;
-    if (header) {
-        // if present, the header is where you move the DIV from:
-        header!.onmousedown = dragMouseDown;
-    } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        dialog.onmousedown = dragMouseDown;
-    }
+import { Vector } from "./Vector";
 
-    function dragMouseDown(e: MouseEvent) {
-        e = e || window.event;
+const dragElement = <T extends HTMLElement>(dialog: T): T => {
+    const lastPos: Vector = new Vector();
+    const dragMouseDown = (e: MouseEvent) => {
         e.preventDefault();
         dialog.style.zIndex = '1';
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+        lastPos.x = e.clientX;
+        lastPos.y = e.clientY;
+        document.addEventListener('mouseup', closeDragElement);
+        document.addEventListener('mousemove', elementDrag);
     }
 
-    function elementDrag(e) {
-        e = e || window.event;
+    const elementDrag = (e: MouseEvent) => {
         e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        dialog.style.top = (dialog.offsetTop - pos2) + "px";
-        dialog.style.left = (dialog.offsetLeft - pos1) + "px";
+        const dx = lastPos.x - e.clientX;
+        const dy = lastPos.y - e.clientY;
+        lastPos.x = e.clientX;
+        lastPos.y = e.clientY;
+        dialog.style.top = (dialog.offsetTop - dy) + "px";
+        dialog.style.left = (dialog.offsetLeft - dx) + "px";
     }
 
-    function closeDragElement() {
-        // stop moving when mouse button is released:
+    const closeDragElement = () => {
         dialog.style.zIndex = '0';
-        document.onmouseup = null;
-        document.onmousemove = null;
+        document.removeEventListener('mouseup', closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
     }
+
+    const header = dialog.querySelector<HTMLDivElement>(".dialog-header")!;
+    if (header) {
+        header!.addEventListener('mousedown', dragMouseDown);
+    } else {
+        dialog.addEventListener('mousedown', dragMouseDown);
+    }
+
     return dialog;
 }
+
+export default dragElement;
